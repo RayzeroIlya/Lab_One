aef3b6c4f4f92a2ef64bf5c308eb7f970ddf6f59
Ilya Ryzhov <raythezero2905@yandex.ru>
2024-03-24 16:43:02 +0700

    updated Rectaangle.kt

--- kotlin/src/main/kotlin/Rectangle.kt
+++ kotlin/src/main/kotlin/Rectangle.kt
@@ -2,6 +2,6 @@ package org.example
 
 import kotlin.math.sqrt
 
-fun rectangleSquare(width : Double , length : Double) : Double = width*length
+fun rectangleSquare(width : Double , length : Double)  = width*length
 fun rectanglePerimeter(width : Double , length : Double) = (width+length)*2
 fun rectangleDiagonal(width : Double , length : Double) = sqrt(length*length+width*width)
