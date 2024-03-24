42c31e6edd3a320b735212de69e445a4aef2aa0b
Ilya Ryzhov <raythezero2905@yandex.ru>
2024-03-24 16:41:00 +0700

    updated lab_one.cpp and Main.kt

--- Lab_One/Lab_One.cpp
+++ Lab_One/Lab_One.cpp
@@ -1,11 +1,17 @@
 ﻿
 #include <iostream>
+#include "Rectangle.h"
 
 using namespace std;
 
 int main()
 {
-
+	int length, width;
+	cout << "Input lenght and width for rectangle\n";
+	cin >> length >> width;
+	cout << "Perimeter = " << rectanglePerimeter(length, width) << endl;
+	cout << "Square = " << rectangleSquare(length, width) << endl;
+	cout << "Diagonal = " << rectangleDiagonal(length, width) << endl;
 }
 
 
--- kotlin/src/main/kotlin/Main.kt
+++ kotlin/src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ package org.example
 fun main() {
     val length: Double = readln().toDouble()
     val width:Double = readln().toDouble()
-    println(rectangleSquare(width, length))
-    println(rectangleDiagonal(width, length))
-    println(rectanglePerimeter(width, length))
+    println("Square = ${rectangleSquare(width, length)}")
+    println("Perimeter = ${rectangleDiagonal(width, length)}")
+    println("Diagonal = ${rectanglePerimeter(width, length)}")
 }
