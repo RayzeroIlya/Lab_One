e5e68afd8ddb7f41e33133e2a7b7324d82d4adbe
Ilya Ryzhov <raythezero2905@yandex.ru>
2024-03-24 13:53:42 +0700

    cleared project

--- Lab_One/Rectangle.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-double rectangleSquare(double length, double width);
-double rectanglePerimeter(double length, double width);
-double rectangleDiagonal(double length, double width);
--- Lab_One/Triangle.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-double trianglePerimeter(double sideA, double sideB, double sideC);
-double triangleSquare(double sideA, double sideB, double sideC);
-int isoscelesTriangle(double sideA, double sideB, double sideC);
